0d4af212d9a9fe4ea1ee1a962673104a4954ccfa
Thymus <thymus@pop-os.localdomain>
2022-01-08 08:48:11 +0100

    initialise with a quote

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import {useState} from "react";
 import Quotecard from "./components/Quotecard";
 
 function App() {
-  const [quote,setQuote]= useState('');
+    const initQuote={quote:"Shut up, brain. I got friends now. I don't need you anymore.",
+                    character:"Lisa Simpson",
+                    image:"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083",
+    }
+  const [quote,setQuote]= useState(initQuote);
   const getQuote= () => {
     axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
         .then((reponse) => reponse.data)
